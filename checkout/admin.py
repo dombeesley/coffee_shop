648b16569432111f199d1db877f3b7e58400cb1e
dombeesley <quigley_beesley@hotmail.com>
2020-06-18 10:32:42 +0000

    update models and admin in checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+class OrderLineAdminInline(admin.TabularInline):
+    model = OrderLineItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineAdminInline, )
+
+
+admin.site.register(Order, OrderAdmin)
