66a41409d63a954122a265828049aa19a56067bc
dombeesley <quigley_beesley@hotmail.com>
2020-06-25 08:18:57 +0000

    add new line to end of urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^login/', login, name="login"),
     url(r'^register/', registration, name="register"),
     url(r'^profile/', user_profile, name="profile"),
-    url(r'^password_reset/', include(url_reset))
+    url(r'^password_reset/', include(url_reset)),
 ]
