985a781ffe46bbf39b38684f1b0b259ec59d93f5
dombeesley <quigley_beesley@hotmail.com>
2020-06-27 11:57:39 +0000

    add media to s3

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -147,7 +147,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
@@ -155,6 +156,8 @@ STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
