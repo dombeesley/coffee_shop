33441dae98d5777e56ca17d408457513480851bb
dombeesley <quigley_beesley@hotmail.com>
2020-07-09 09:21:41 +0000

    add comment to views.py file

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Product
 
-# Display all products
+
 def all_products(request):
+    """Displays all products available"""
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
