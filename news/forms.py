356915c41ece220ee208ea3bda740aa013a8203b
dombeesley <quigley_beesley@hotmail.com>
2020-07-09 14:22:28 +0000

    update news app

--- news/forms.py
+++ news/forms.py
@@ -1,9 +1,6 @@
-from django import forms
+from django.contrib import admin
 from .models import Post
 
+# Register your models here.
 
-class BlogPostForm(forms.ModelForm):
-
-    class Meta:
-        model = Post
-        fields = ('title', 'content', 'image', 'tag', 'published_date')
+admin.site.register(Post)
